f79209d2109e538b9240fdf0c8aa03d8e48dcf7e
Gianpierre <fgianpierre@gmail.com>
2021-10-13 20:54:46 +0200

    add library reat-gtm-module to handle the google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import App from 'next/app'
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
-import React from 'react'
+import React, {useEffect} from 'react'
 import type {AppProps, AppContext} from 'next/app'
 import {AppStateProvider} from '../context'
 import {SessionProvider} from 'next-auth/react'
 import ThemeWrapper from '@components/ThemeWrapper'
 import Layout from '@components/layout'
+import TagManager from 'react-gtm-module'
+import {NEXT_PUBLIC_GTM_ID} from 'config'
+
+const tagManagerArgs = {
+  gtmId: NEXT_PUBLIC_GTM_ID as string,
+}
 
 function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return (
     <ThemeWrapper>
       <SessionProvider session={session}>
