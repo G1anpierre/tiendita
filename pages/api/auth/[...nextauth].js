04b8c89767610ed37fb3f432d7cb38a0c6b68a45
Gianpierre <fgianpierre@gmail.com>
2021-10-15 15:31:33 +0200

    update env vars

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,10 @@ import GoogleProvider from 'next-auth/providers/google'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import {
+  NEXT_PUBLIC_GITHUB_CLIENT_ID,
+  NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+} from './config'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,8 +16,8 @@ export default NextAuth({
 
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: NEXT_PUBLIC_GITHUB_CLIENT_ID,
+      clientSecret: NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
