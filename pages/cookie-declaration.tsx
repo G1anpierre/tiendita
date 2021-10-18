1ffcae0220dca71a6d1205b2f0bb1c47aeb9346c
Gianpierre <fgianpierre@gmail.com>
2021-10-18 23:04:29 +0200

    add cookie-declaration

---
+++ pages/cookie-declaration.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CookieDeclaration = () => {
+  return (
+    <div>
+      <h1>Cookie Declaration</h1>
+      <script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/7b132f08-cd18-475c-a995-2a7991985675/cd.js"
+        type="text/javascript"
+        async
+      ></script>
+    </div>
+  )
+}
+
+export default CookieDeclaration
