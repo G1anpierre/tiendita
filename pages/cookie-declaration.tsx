63cb810e51ff9f0cd3b062e0242e0390cbea33b9
Gianpierre <fgianpierre@gmail.com>
2021-10-20 00:46:56 +0200

    script next cookie declaratioin

--- pages/cookie-declaration.tsx
+++ pages/cookie-declaration.tsx
@@ -1,20 +1,18 @@
 import React, {useEffect} from 'react'
 import {useRouter} from 'next/router'
+import Script from 'next/script'
+
 import Head from 'next/head'
 
 const CookieDeclaration = () => {
   return (
     <>
-      <Head>
-        <script
-          id="CookieDeclaration"
-          src="https://consent.cookiebot.com/7b132f08-cd18-475c-a995-2a7991985675/cd.js"
-          type="text/javascript"
-          async
-        ></script>
-      </Head>
       <div>
         <h1>Cookie Declaration 1</h1>
+        <Script
+          id="CookieDeclaration"
+          src="https://consent.cookiebot.com/7b132f08-cd18-475c-a995-2a7991985675/cd.js"
+        />
       </div>
     </>
   )
