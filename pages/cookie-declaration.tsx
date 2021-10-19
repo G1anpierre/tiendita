a305058a4f8963229951f245ea8bdfce95baf496
Gianpierre <fgianpierre@gmail.com>
2021-10-20 00:12:19 +0200

    fix head

--- pages/cookie-declaration.tsx
+++ pages/cookie-declaration.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {useRouter} from 'next/router'
-import Head from 'next/Head'
+import Head from 'next/head'
 
 const CookieDeclaration = () => {
   return (
