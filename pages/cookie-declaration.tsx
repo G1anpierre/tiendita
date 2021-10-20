7161e3a14ea0c854933b77ede94d92ac5bab6ad2
Gianpierre <fgianpierre@gmail.com>
2021-10-20 21:04:23 +0200

    append code when chanching routes

--- pages/cookie-declaration.tsx
+++ pages/cookie-declaration.tsx
@@ -8,7 +8,7 @@ const CookieDeclaration = () => {
   return (
     <>
       <h1>Cookie Declaration</h1>
-      <div id="policy-script">Here is going to come code</div>
+      <div id="policy-script"></div>
       <Script id="show-banner" strategy="afterInteractive">
         {`
         window.onload = function() {
@@ -22,18 +22,6 @@ const CookieDeclaration = () => {
         }
         `}
       </Script>
-      {/* <Script
-        strategy="afterInteractive"
-        onLoad={() => {
-          console.log('hello world')
-          const script = document.createElement('script')
-          script.src =
-            'https://consent.cookiebot.com/7b132f08-cd18-475c-a995-2a7991985675/cd.js'
-          script.setAttribute('id', 'CookieDeclaration')
-          console.log('script: ', script)
-          document.getElementById('policy-script')?.appendChild(script)
-        }}
-      /> */}
     </>
   )
 }
