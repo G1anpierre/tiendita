24b1ce48d1277253b57179dd5f76950ffae52737
Gianpierre <fgianpierre@gmail.com>
2021-10-20 21:09:59 +0200

    clean unneserary console.logs

--- pages/cookie-declaration.tsx
+++ pages/cookie-declaration.tsx
@@ -1,10 +1,7 @@
-import React, {useEffect} from 'react'
-import {useRouter} from 'next/router'
+import React from 'react'
 import Script from 'next/script'
 
 const CookieDeclaration = () => {
-  const router = useRouter()
-
   return (
     <>
       <h1>Cookie Declaration</h1>
@@ -16,8 +13,6 @@ const CookieDeclaration = () => {
           script.src =
             'https://consent.cookiebot.com/7b132f08-cd18-475c-a995-2a7991985675/cd.js'
           script.setAttribute('id', 'CookieDeclaration')
-          console.log('script: ', script)
-          console.log('policy-script: ', document.getElementById('policy-script'))
           document.getElementById('policy-script')?.appendChild(script)
         }
         `}
