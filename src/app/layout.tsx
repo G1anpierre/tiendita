c4ac95e9f23be0f3639249b3e721ff8a8a711d33
G1anpierre <fgianpierre@gmail.com>
2024-02-08 22:40:20 +0100

    fix: update paths aliases configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,14 @@ import {Providers} from 'src/providers'
 import './styles/globals.css'
 import {Header} from '@components/Header'
 import {Footer} from '@components/Footer'
+import {Inter} from 'next/font/google'
+
+const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>
+      <body className={`${inter.className}`} suppressHydrationWarning>
         <Providers>
           <Header />
           {children}
