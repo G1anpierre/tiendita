2671fdbf7413970c01680d17c4e054922a58bfaa
G1anpierre <fgianpierre@gmail.com>
2024-02-08 23:12:00 +0100

    fix: update components.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import './styles/globals.css'
 
 import {Inter} from 'next/font/google'
 
-import {Footer} from '../components/Footer'
-import {Header} from '../components/Header'
+import {Footer} from '@/components/Footer'
+import {Header} from '@/components/Header'
 import {Providers} from '@/providers'
 
 const inter = Inter({subsets: ['latin']})
